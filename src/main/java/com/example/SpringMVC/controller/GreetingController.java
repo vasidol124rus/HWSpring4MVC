ee598b4f0669f52bdf2987ed9b72a5b2a4d37185
Vasiliy Kuznetsov <vasekku25@gmail.com>
2024-11-13 15:52:51 +0700

    End

--- src/main/java/com/example/SpringMVC/controller/GreetingController.java
+++ src/main/java/com/example/SpringMVC/controller/GreetingController.java
@@ -1,16 +1,28 @@
 package com.example.SpringMVC.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping
 public class GreetingController {
 
     @GetMapping
-    public String greeting() {
+    public String greeting(Model model) {
+        model.addAttribute("name", "Иван");
+        model.addAttribute("age", 25);
         return "index";
+    }
+    @PostMapping("/process")
+    public String process(Model model, @RequestParam("name") String name,
+                          @RequestParam("age") int age) {
+        model.addAttribute("name", name);
+        model.addAttribute("age", age);
+        return "result";
     }
 
 }
